587ea57f482daf19e5d8e676003459423b08980f
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-12-04 16:56:12 +0100

    push 1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './/components/AppHeader.vue'
+import store from './store'
+
+data (){
+  return {
+    store
+  }
+}
+
+
+</script>
+
+<template>
+    
+</template>
+
+<style scoped>
+
+</style>
+
+mia chiave API: f2968eda5637985218434bb70e4158f3
